fcfc5a6a2117960512377fa52c6b8d9e4061f775
Tor Nordqvist <tornord@gmail.com>
2024-12-01 06:31:56 +0100

    dec-1

--- days/2024/1/index.js
+++ days/2024/1/index.js
@@ -1,51 +1,32 @@
-import { strict as assert } from "node:assert"; // eslint-disable-line
+import { countBy, parseTable } from "../../../common";
 
-import { sum, prod, range, newArray, newMatrix, transpose, isNumeric } from "../../../common"; // eslint-disable-line
-import { intersectionSet, reduceSet, unionSet } from "../../../common"; // eslint-disable-line
-import { toDict, groupBy, countBy, uniquePermutations, indexOf } from "../../../common"; // eslint-disable-line
-import { matchNumbers, parseTable, splitArray } from "../../../common" // eslint-disable-line
-
-const { abs, ceil, floor, max, min, random, round, sign, sqrt } = Math; // eslint-disable-line
-const { isArray } = Array; // eslint-disable-line
+const { abs } = Math;
 
 function calc1(input) {
-  const ny = input.length;
-  const nx = input?.[0]?.length ?? 0;
-  for (let y = 0; y < ny; y++) {
-    let r = input[y]; // eslint-disable-line
-    for (let x = 0; x < nx; x++) {
-      //
-    }
+  const list1 = input.map((r) => r[0]).toSorted((a, b) => a - b);
+  const list2 = input.map((r) => r[1]).toSorted((a, b) => a - b);
+  let res = 0;
+  for (let y = 0; y < input.length; y++) {
+    const c1 = list1[y];
+    const c2 = list2[y];
+    res += abs(c1 - c2);
   }
-  return 0;
+  return res;
 }
 
 function calc2(input) {
-  const ny = input.length;
-  const nx = input?.[0]?.length ?? 0;
-  for (let y = 0; y < ny; y++) {
-    let r = input[y]; // eslint-disable-line
-    for (let x = 0; x < nx; x++) {
-      //
-    }
+  const list1 = input.map((r) => r[0]);
+  const list2 = input.map((r) => r[1]);
+  const g2 = countBy(list2, (r) => r);
+  let res = 0;
+  for (let y = 0; y < input.length; y++) {
+    const c1 = list1[y];
+    res += c1 * (g2[c1] ?? +0);
   }
-  return 0;
+  return res;
 }
 
 export default function (inputRows) {
-  let input1 = parseTable(inputRows);
-  let input2 = splitArray(inputRows, (r) => r === "");
-  let input3 = inputRows.map(matchNumbers);
-  let input4 = inputRows.map((r) => r.split(/, ?/g));
-  let input5 = inputRows.map((r) => r.split(""));
-  let input = input1;
+  const input = parseTable(inputRows);
   return [calc1(input), calc2(input)];
 }
-
-// (r) => r.split(/[-]|: | /g);
-// (r) => r.split(" ");
-// (r) => r.split(" ").map((d, i) => (i === 0 ? d : Number(d)));
-// (r) => r.split(" ").join("");
-// (r) => r.split(/ \| /).map((d) => d.split(" "));
-// (r) => r.split("").map(Number);
-// (r) => r.split(/[,-]/g).map(Number);
